723c1f4a324bd11127241e45c960e0a80c7e35b8
hainiu <hainiu@example.com>
2023-04-21 14:02:47 +0800

    Revert "javase阶段所有代码v4版本 修改了一个Demo类"
    This reverts commit 4c15feda

--- day12/src/com/hainiu/demo.java
+++ day12/src/com/hainiu/demo.java
@@ -8,7 +8,5 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-        System.out.println("java");
-        System.out.println("Git");
     }
 }
