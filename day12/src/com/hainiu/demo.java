eb2634a20aa4e259789718903332d0e16d1457c1
hainiu <hainiu@example.com>
2023-04-21 13:50:45 +0800

    javase阶段所有代码v2版本 添加了一个Demo类

---
+++ day12/src/com/hainiu/demo.java
@@ -0,0 +1,12 @@
+package com.hainiu;
+
+/**
+ * @author DaVid
+ * @create 2023-04-21-13:49
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+    }
+}
