c92a2c0c17033c184180f8968217e3203e2387fb
hainiu <hainiu@example.com>
2023-05-02 20:56:26 +0800

    debug分支修改了Demo类

--- day12/src/com/hainiu/demo.java
+++ day12/src/com/hainiu/demo.java
@@ -8,5 +8,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+
+        System.out.println("debug");
     }
 }
